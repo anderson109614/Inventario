3b05ae7257359bb6f5e591a73378cea5661d0aee
anderson109614 <anderson109614@gmail.com>
2019-08-15 10:39:46 -0500

    ruteado suministros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { ListabienesComponent } from './components/listabienes/listabienes.component';
 import { BienesactualizarComponent } from './components/bienesactualizar/bienesactualizar.component';
 import { BienesnuevoComponent } from './components/bienesnuevo/bienesnuevo.component';
-
+import { SuministrosComponent} from './components/suministros/suministros.component';
 
 const routes: Routes = [
   {path:'listabienes', component: ListabienesComponent},
   {path:'bienesactualizar/:id', component: BienesactualizarComponent},
-  {path:'bienesnuevo', component: BienesnuevoComponent}
+  {path:'bienesnuevo', component: BienesnuevoComponent},
+  {path:'suministros', component: SuministrosComponent}
 ];
 
 @NgModule({
