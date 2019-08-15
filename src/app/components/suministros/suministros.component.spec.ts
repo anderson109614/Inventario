f7f25f456555ac600ee6f0c4a05a5025fcfa6f56
anderson109614 <anderson109614@gmail.com>
2019-08-15 10:34:37 -0500

    creado suministros

---
+++ src/app/components/suministros/suministros.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SuministrosComponent } from './suministros.component';
+
+describe('SuministrosComponent', () => {
+  let component: SuministrosComponent;
+  let fixture: ComponentFixture<SuministrosComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SuministrosComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SuministrosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/suministros/suministros.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-suministros',
+  templateUrl: './suministros.component.html',
+  styleUrls: ['./suministros.component.css']
+})
+export class SuministrosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
