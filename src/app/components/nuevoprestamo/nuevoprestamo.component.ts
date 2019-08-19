cf3a044df0ad0c98cdcf3572b73013f36c7f6d32
anderson109614 <anderson109614@gmail.com>
2019-08-19 17:03:29 -0500

    nuevo prestamo

---
+++ src/app/components/nuevoprestamo/nuevoprestamo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nuevoprestamo',
+  templateUrl: './nuevoprestamo.component.html',
+  styleUrls: ['./nuevoprestamo.component.css']
+})
+export class NuevoprestamoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
